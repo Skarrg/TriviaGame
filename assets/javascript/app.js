684a8c5fa51d4b767166ac586fedecfebd29ded4
Skarrggit config --global user.name Skarrggit config --global user.name Skarrg <orestes.moiran@gmail.com>
2018-10-19 21:11:49 -0700

    And now it looks good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     function setup() {
         numberAnswered = 0;
         $('.answer').hide();
-        $('.question').append('<button id="startButton"> TriviaTime! </button>');
+        $('.question').append('<button type="button" class ="btn btn-block" id="startButton"> Trivia Time! </button>');
         $('#startButton').on('click', function() {
             $(this).hide();
             timer.start();
